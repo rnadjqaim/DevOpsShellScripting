fcd71c2e49e8a489c46fa100a48f08eba3ecbaaa
Rnad Abu Jqaim <103530494+rnadjqaim@users.noreply.github.com>
2022-08-21 10:42:45 +0300

    Create Jenkins-Versioning.sh

---
+++ Jenkins-Versioning.sh
@@ -0,0 +1,8 @@
+# when run the job, the new  artifact will replacethe old artifact & and deploy new artifiact
+# if you want to roll back to previous artifacts, it wad overwritten is gone .
+# put this script in jenkins job when we build it
+mkdir -p versions 
+cp target/vprofile-v2.war versions/vprofile-V$BUILD_ID.war
+#  jenkins have many variables  : https://phoenixnap.com/kb/jenkins-environment-variables#ftoc-heading-1
+#adding some variables 
+
